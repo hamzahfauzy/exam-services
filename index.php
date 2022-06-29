8aa246068703f871d36d5864586096ec43936807
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-06-29 14:25:33 +0700

    add cors

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 header('content-type: application/json'); 
 
 require 'config.php';
