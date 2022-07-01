59a1aee8e6116098b41f109a3921bd0e3084d871
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-07-01 11:31:45 +0700

    update modules

--- modules.php
+++ modules.php
@@ -16,7 +16,7 @@ return [
     'sequences/index'  => 'GET',
     'sequences/start'  => 'POST',
     'sequences/answer' => 'POST',
-    'sequences/next'   => 'POST',
+    'sequences/save-category-index'   => 'POST',
     'sequences/finish' => 'POST',
 
     'utils/delete' => 'DELETE',
