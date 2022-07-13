33429e00b55d05cd362c497fe162b2dbf7547d50
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-07-13 08:51:29 +0700

    add user agent

--- modules/auth/login.php
+++ modules/auth/login.php
@@ -13,9 +13,10 @@ if(!isset($_POST['username']) || empty($_POST['username']))
 
 $conn = getConnection();
 
-$username = $_POST['username'];
+$username   = $_POST['username'];
+$user_agent = $conn->real_escape_string($_POST['user_agent']);
 
-$query    = $conn->query("SELECT * FROM user WHERE username='$username'");
+$query      = $conn->query("SELECT * FROM user WHERE username='$username'");
 
 if(!$query->num_rows)
 {
@@ -24,7 +25,7 @@ if(!$query->num_rows)
 
 $result   = $query->fetch_object();
 $auth_key = generateRandomString();
-$conn->query("UPDATE user set auth_key = '$auth_key' WHERE id = $result->id");
+$conn->query("UPDATE user set auth_key = '$auth_key', user_agent = '$user_agent' WHERE id = $result->id");
 
 return response('success','user valid', [
     'auth_key' => $auth_key
